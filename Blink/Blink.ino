c4b11a9b5ca1dec4ae849f613fd521747400212a
sas26031962 <sasx26031962@gmail.com>
2025-11-10 20:13:35 +0300

    Добавление документации и скетча Blink

---
+++ Blink/Blink.ino
@@ -0,0 +1,25 @@
+/*
+  Blink
+
+  Программа мигания встроенным светодиодом для платы Raspberry Pi  Pico
+  Светодиод подключён к выводу D25
+*/
+
+#define LED_PIN 25
+#define DWELL 500
+
+// the setup function runs once when you press reset or power the board
+void setup() 
+{
+  // initialize digital pin LED_PIN as an output.
+  pinMode(LED_PIN, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() 
+{
+  digitalWrite(LED_PIN, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(DWELL);                 // wait for a second
+  digitalWrite(LED_PIN, LOW);   // turn the LED off by making the voltage LOW
+  delay(DWELL);                 // wait for a second
+}
